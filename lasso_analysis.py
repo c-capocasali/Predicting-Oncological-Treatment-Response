a827a6e33861e423c06b82f4013a1638054fc0a7
c-capocasali <caio.capocasali@usp.br>
2026-08-22 20:07:36 -0300

    correção de bugs

--- lasso_analysis.py
+++ lasso_analysis.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import StandardScaler, FunctionTransformer
 from sklearn.model_selection import train_test_split, GridSearchCV, KFold
 from sksurv.linear_model import CoxnetSurvivalAnalysis
 from sklearn.model_selection import StratifiedKFold
+from sklearn.feature_selection import VarianceThreshold
 
 
 def lasso(target_file):
@@ -12,7 +13,7 @@ def lasso(target_file):
     # Carrega arquivo parquet
     df = pl.read_parquet(target_file)
 
-    target_cols = ['survival_time', 'event']
+    target_cols = ['survival_time', 'event', 'case_id', 'project']
     gene_cols = [col for col in df.columns if col not in target_cols]
 
     X = df.select(gene_cols).to_pandas()
@@ -30,10 +31,12 @@ def lasso(target_file):
 
 # Transformador Log(x+1)
     log_transformer = FunctionTransformer(np.log1p, validate=False)
+    var_filter = VarianceThreshold(threshold=0.1)
 
 # Pipeline Batedor: Descobre a faixa de alphas ideal para esses dados
     pipeline_batedor = Pipeline([
         ('log', log_transformer),
+        ('var_filter', var_filter),
         ('scaler', StandardScaler()),
         ('cox_lasso', CoxnetSurvivalAnalysis(l1_ratio=1.0))
     ])
@@ -46,18 +49,20 @@ def lasso(target_file):
 # Pipeline Final (com fit_baseline_model=True)
     pipeline_final = Pipeline([
         ('log', log_transformer),
+        ('var_filter', var_filter),
         ('scaler', StandardScaler()),
         ('cox_lasso', CoxnetSurvivalAnalysis(l1_ratio=1.0, fit_baseline_model=True))
     ])
 
 # K-Fold CV
-    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    cv_splits = list(skf.split(X_train, y_train['Status']))
 
 # GridSearch configurado com error_score=0.5
     gcv = GridSearchCV(
         pipeline_final,
         param_grid=param_grid,
-        cv=cv,
+        cv=cv_splits,
         n_jobs=-1,
         error_score=0.5
     )
@@ -69,13 +74,16 @@ def lasso(target_file):
     print(f"\n[Treino/CV] Melhor Alpha encontrado: {melhor_alpha:.6f}")
     print(f"[Treino/CV] C-Index médio na Validação Cruzada: {c_index_val:.4f}")
 
+    genes_pos_variancia = X_train.columns[gcv.best_estimator_.named_steps['var_filter'].get_support(
+    )]
+
 # O método .score() do scikit-survival calcula automaticamente o Harrell's C-index
     c_index_teste = gcv.score(X_test, y_test)
 
     melhor_modelo_cox = gcv.best_estimator_.named_steps['cox_lasso']
     coeficientes_finais = melhor_modelo_cox.coef_[:, 0]
 
-    genes_selecionados = X_train.columns[coeficientes_finais != 0].tolist()
+    genes_selecionados = genes_pos_variancia[coeficientes_finais != 0].tolist()
 
     print(f"\nTotal de genes sobreviventes ao Lasso: {
           len(genes_selecionados)}")
