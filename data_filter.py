d4b6303f11dd66e92a6edc9264d2b52ed5be9be2
c-capocasali <caio.capocasali@usp.br>
2026-08-22 08:12:39 -0300

    first commit

---
+++ data_filter.py
@@ -0,0 +1,21 @@
+import marimo
+
+__generated_with = "0.23.16"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import polars as pd 
+    import numpy as np
+
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
